3039b99653e09c4b4e7f44550058a2dcf0c818b4
Laurion Dareich <klauriondareich@gmail.com>
2019-09-09 00:12:01 +0100

    initial state

---
+++ the-world/assets/js/file.js
@@ -0,0 +1,28 @@
+  let continent = document.querySelectorAll("li");
+
+  continent.forEach(element =>{
+    console.log(element);
+    element.addEventListener("click",isContinentSelected);
+  });
+
+  function isContinentSelected(){
+    this.dataset.selected = this.dataset.selected === "true" ? "false":"true";
+
+    numberOfSelectedElement();
+    selectedContinent();
+  };
+
+  function numberOfSelectedElement(){
+    let selectedElt = document.querySelectorAll("li[data-selected='true']").length;
+
+    document.querySelector(".last-block span:nth-child(2)").textContent = "Nombre : " + selectedElt;
+  }
+
+  function selectedContinent(){
+    let isTrueArray = document.querySelectorAll("li[data-selected='true']");
+    let displayInArray = [];
+    isTrueArray.forEach(elt =>{
+      displayInArray.push(elt.textContent);
+    })
+    document.querySelector(".last-block span:first-child").textContent = displayInArray.join(", ");
+   }
